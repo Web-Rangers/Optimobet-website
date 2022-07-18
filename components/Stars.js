5ac742d6423b498de337c44355926d1623931dd4
NikitoriusMinsk <45353693+NikitoriusMinsk@users.noreply.github.com>
2022-07-18 12:47:11 +0400

    stars 5 > 1 on mobile

--- components/Stars.js
+++ components/Stars.js
@@ -1,39 +1,44 @@
 import { ReactSVG } from 'react-svg'
 import { useEffect, useState } from 'react'
 import styles from '/styles/components/Stars.module.css'
+import useWindowSize from '../hooks/useWindowSize'
 
-export default function Stars({points=0}) {
+export default function Stars({ points = 0 }) {
     const [pointsS, setPoinstS] = useState(points)
+    const { width } = useWindowSize()
 
-    useEffect(()=>{
+    useEffect(() => {
         setPoinstS(points)
-    },[points])
+    }, [points])
 
     return (
         <>
             <ReactSVG
-              src='/images/icons/star.svg'
-              className={`${styles.star} ${pointsS>=0.5 && styles.activeStar}`}
+                src='/images/icons/star.svg'
+                className={`${styles.star} ${pointsS >= 0.5 && styles.activeStar}`}
             />
-            <ReactSVG
-              src='/images/icons/star.svg'
-              className={`${styles.star} ${pointsS>=1.5 && styles.activeStar}`}
-            />
-            <ReactSVG
-              src='/images/icons/star.svg'
-              className={`${styles.star} ${pointsS>=2.5 && styles.activeStar}`}
-            />
-            <ReactSVG
-              src='/images/icons/star.svg'
-              className={`${styles.star} ${pointsS>=3.5 && styles.activeStar}`}
-            />
-            <ReactSVG
-              src='/images/icons/star.svg'
-              className={`${styles.star} ${pointsS>=4.5 && styles.activeStar}`}
-            />
+            {width > 480 && <>
+                <ReactSVG
+                    src='/images/icons/star.svg'
+                    className={`${styles.star} ${pointsS >= 1.5 && styles.activeStar}`}
+                />
+                <ReactSVG
+                    src='/images/icons/star.svg'
+                    className={`${styles.star} ${pointsS >= 2.5 && styles.activeStar}`}
+                />
+                <ReactSVG
+                    src='/images/icons/star.svg'
+                    className={`${styles.star} ${pointsS >= 3.5 && styles.activeStar}`}
+                />
+                <ReactSVG
+                    src='/images/icons/star.svg'
+                    className={`${styles.star} ${pointsS >= 4.5 && styles.activeStar}`}
+                />
+            </>
+            }
             <span className={styles.points}>
                 {pointsS}
             </span>
-        </>        
+        </>
     )
 }
